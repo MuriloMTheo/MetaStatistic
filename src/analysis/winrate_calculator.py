186dd6caeb6f4616191093a98653ae29d6efa3ff
Murilo Moreira <murilomoreira20123@hotmail.com>
2026-05-03 21:16:49 -0300

    AdicionandoSettings

--- src/analysis/winrate_calculator.py
+++ src/analysis/winrate_calculator.py
@@ -1,11 +1,27 @@
 import duckdb
+from src.config.settings import DB_PATH, MIN_GAMES_FOR_TIER
 
-con = duckdb.connect("data/meta_db/lol.duckdb")
+def get_raw_stats (db_path: str):
+    con = duckdb.connect(db_path)
 
-df = con.execute("""
-    SELECT PlayerChampion, Lane, Count(*) AS Games, SUM(Win) AS Wins
-    FROM PARTIDAS_GERAL
-    GROUP BY PlayerChampion, Lane              
-""").df()
+    df = con.execute("""
+        SELECT PlayerChampion, Lane, Count(*) AS Games, SUM(Win) AS Wins
+        FROM PARTIDAS_GERAL
+        GROUP BY PlayerChampion, Lane              
+    """).df()
 
-con.close()
+    con.close()
+    return df
+    
+def calculate_winrate(df):
+    df = df[(df["Games"] > MIN_GAMES_FOR_TIER)]
+    df["Winrate"] = df["Wins"] / df["Games"]
+    return df
+
+if __name__ == "__main__": #testelocal
+    dfw = get_raw_stats(DB_PATH)
+    dfw = calculate_winrate(dfw)
+    print(dfw.head(10))
+
+#print(df[df["PlayerChampion"] == 'Lux'].head())
+#print(df["Lane"].unique())
---
+++ src/config/settings.py
@@ -0,0 +1,2 @@
+DB_PATH = "data/meta_db/lol.duckdb"
+MIN_GAMES_FOR_TIER = 20
