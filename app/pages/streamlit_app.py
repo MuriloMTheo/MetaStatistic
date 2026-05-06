66a9f31769e469d5407be002fdfe02f8b53f41fe
Murilo Moreira <murilomoreira20123@hotmail.com>
2026-05-06 18:29:09 -0300

    PaginaTier

--- app/pages/streamlit_app.py
+++ app/pages/streamlit_app.py
@@ -1,11 +1,59 @@
 import streamlit as st
+from src.services.tierlist_service import TierListService
+from src.config.settings import TIER_COLORS
 
 st.set_page_config(page_title="Lich Bane Meta", layout="wide")
 
-aba_campeoes, aba_select, aba_skins = st.tabs(["Campeões", "Champion Select", "Champion Skins"])
+aba_campeoes, aba_select, aba_skins = st.tabs(
+    ["Campeões", "Champion Select", "Champion Skins"]
+)
 
 with aba_campeoes:
-    st.write("aq vai a tierlist")
+
+    if "position" not in st.session_state:
+        st.session_state.position = "ALL"
+
+    allchamp, top, jg, mid, adc, sup = st.columns(6)
+
+    with allchamp:
+        if st.button("TODOS"):
+            st.session_state.position = "ALL"
+    with top:
+        if st.button("TOP"):
+            st.session_state.position = "TOP"
+    with jg:
+        if st.button("JUNGLE"):
+            st.session_state.position = "JUNGLE"
+    with mid:
+        if st.button("MID"):
+            st.session_state.position = "MIDDLE"
+    with adc:
+        if st.button("ADCARRY"):
+            st.session_state.position = "BOTTOM"
+    with sup:
+        if st.button("SUPORTE"):
+            st.session_state.position = "SUPPORT"
+
+    # st.write(st.session_state.position)
+    service = TierListService()
+    df = service.get_tier_list(st.session_state.position)
+
+    df["WilsonScore"] = (df["WilsonScore"] * 100).round(2)
+    df = df[["PlayerChampion", "WilsonScore", "Tier"]]
+    df = df.rename(columns={"WilsonScore": "Winrate", "PlayerChampion": "Campeão"})
+
+    for _, row in df.iterrows():
+        col1, col2, col3, col4, col5 = st.columns([1, 2, 1, 1, 1])
+        with col1:
+            st.write("ícone champ")
+        with col2:
+            st.write(row["Campeão"])
+        with col3:
+            st.write("ícone lane")
+        with col4:
+            st.write(f"{row['Winrate']}%")
+        with col5:
+            st.write(row["Tier"])
 
 with aba_select:
     st.write("aq vai o champion select")
--- src/config/settings.py
+++ src/config/settings.py
@@ -1,3 +1,11 @@
 DB_PATH = "data/meta_db/lol.duckdb"
 MIN_GAMES_FOR_TIER = 20
 MIN_LANE_RATIO = 0.28
+
+TIER_COLORS = {
+    1: "#A70000",
+    2: "#3E5EEB",
+    3: "#4ECA52",
+    4: "#D6D0CA",
+    5: "#885E4B",
+}
